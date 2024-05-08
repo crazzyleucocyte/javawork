948e65426f5e373d92b5a9d727f7be4d752064ce
crazzyleucocyte <alstjr7696@naver.com>
2024-05-08 18:13:31 +0900

    ..

--- day02/src/_01_casting/T06_Ex03.java
+++ day02/src/_01_casting/T06_Ex03.java
@@ -9,35 +9,35 @@ public class T06_Ex03 {
 //		1. 실습문제
 //		키보드로 부터 하나의 정수 입력받기
 //		그 정수가 음수이면 "음수"를 출력, 음수가 아니면 "음수가 아니다"를 출력하기
-		System.out.print("정수를 입력해주세요. : ");
+		System.out.print("음수와 양수를 판단하기 위해 정수를 입력해주세요. : ");
 		int int1 = s.nextInt();
-		if(int1<0) System.out.println("음수");
-	    if(int1>0) System.out.println("음수가 아니다.");
+		if(int1<0) System.out.println("음수입니다.");
+	    if(int1>0) System.out.println("음수가 아닙니다.");
 
 //		2. 실습문제
 //		키보드로 부터 하나의 정수 입력받기
 //		그 정수가 음수이면 "음수"를 출력,
 //		음수가 아닌 경우 중 0이면 "0이다"를 출력, 0이 아니면 "양수"를 출력
-        System.out.print("정수를 입력해주세요 : ");
+        System.out.print("음수, 양수, 0을 판단하기 위해 정수를 입력해주세요 : ");
         int int2=s.nextInt();
         if(int2<0) {
-	        System.out.println("음수");
+	        System.out.println("음수입니다.");
         } else if(int2==0) {
-	        System.out.println("0이다.");
-        }else {System.out.println("양수");}
+	        System.out.println("0입니다.");
+        }else {System.out.println("양수입니다.");}
 	    
 //		3. 실습문제
 //		키보드로 부터 하나의 정수 입력받기
 //		그 정수가 짝수이면 "짝수"를 출력,
 //		짝수가 아니면 "홀수"를 출력
         
-        System.out.print("정수를 입력해주세요 : ");
+        System.out.print("짝수, 홀수를 판단하기 위해 정수를 입력해주세요 : ");
         int int3=s.nextInt();
         int int3_2=int3%2;
         if (int3_2==0) {
-			System.out.println("짝수");
+			System.out.println("짝수입니다.");
 		}else if (int3_2==1) {
-			System.out.println("홀수");
+			System.out.println("홀수입니다.");
 		}
 
 
@@ -45,6 +45,7 @@ public class T06_Ex03 {
 //		모든 사람이 사탕을 골고루 나눠가지려고 한다
 //		인원 수와 사탕의 개수를 키보드로 입력 받고
 //		1인다 동일하게 나눠가진 사탕 개수와 나눠주고 남은 사탕의 개수를 출력하기
+        System.out.println("사탕을 동일하게 나눠가져가기 위해");
         System.out.print("인원수를 입력해주세요 : ");
         int people = s.nextInt();
         System.out.print("사탕개수를 입력해주세요 : ");
@@ -99,7 +100,7 @@ public class T06_Ex03 {
 //		나이를 키보드로 입력 받아 13세 이하면 "어린이"를 출력
 //		13세초과 ~ 19세 이하이면 "청소년" 출력
 //		19세초과이면 "성인" 출력
-        System.out.print("\n나이를 입력해주세요 : ");
+        System.out.print("\n어린이, 청소년, 성인을 판단하기 위해 나이를 입력해주세요 : ");
         int age = s.nextInt();
         if(age<=13)System.out.println("어린이");
         else if(age>13&&age<=19)System.out.println("청소년");
@@ -138,12 +139,28 @@ public class T06_Ex03 {
 
 //		8. 실습문제
 //		주민번호를 입력받아 나자인지 여자인지 출력하기
-        String id = s.nextLine();
+        do {
+        System.out.print("주민등록 번호를 입력해주세요(-포함): ");
+		String id = s.nextLine();
+		id = s.nextLine();
         char idGender = id.charAt(7);
         char highpen = id.charAt(6);
+        if(id.length()!=14) {
+        	System.out.println("주민등록번호 13자리(-포함)를 올바르게 입력해주세요");
+        	continue;
+        }
+        else if(highpen!='-') {
+        	System.out.println("하이픈(-)을 넣어주세요");
+        	continue;
+        }else if(idGender=='1'||idGender=='3') {
+        	System.out.println("당신은 남자입니다.");
+        	break;
+        }else if(idGender=='2'||idGender=='4') {
+        	System.out.println("당신은 여자입니다.");
+        	break;
+        }
         
-        
-        
+        }while(true);
 
 //		ex.
 //		주민번호를 입력하세요(- 포함) : 123456-3123456
--- day02/src/_02_referenceType/T01_StringClass.java
+++ day02/src/_02_referenceType/T01_StringClass.java
@@ -9,7 +9,7 @@ public class T01_StringClass {
 		System.out.println("주소가 같은가?"+(name == hobby));
 		
 		String str1 = "의료용";
-		String str2 = "영상";
+		String str2 = "의료용";
 		System.out.println("주소가 같은가?"+(str1==str2));
 		
 		String myName = "홍길동";
@@ -18,8 +18,8 @@ public class T01_StringClass {
 		String str3 = new String("영상");
 		String str4 = new String("영상");
 		System.out.println("주소가 같은가?"+(str3==str4));
-		System.out.println("주소가 같은가?"+(str3.equals(str4)));
-		System.out.println("주소가 같은가?"+(str2==str3));
+		System.out.println("값이 같은가?"+(str3.equals(str4)));
+		
 
 	}
 
