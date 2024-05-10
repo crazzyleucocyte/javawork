3cef6a895a192606d9d712db3715b398ceab9750
crazzyleucocyte <alstjr7696@naver.com>
2024-05-10 10:59:59 +0900

    .

---
+++ day04/src/_01_conditional/T01_switch_case.java
@@ -0,0 +1,21 @@
+package _01_conditional;
+
+public class T01_switch_case {
+
+	public static void main(String[] args) {
+
+		int num1 = 1; // 0~2만 들어있는 변수
+		
+		switch(num1) {
+		case 1:
+			System.out.println("1입니다.");
+			break;
+		case 2:
+			System.out.println("2입니다.");
+			break;
+		}
+		
+		
+	}
+
+}
