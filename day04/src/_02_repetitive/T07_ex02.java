29ed330aa7adaf9707a847ae8efe8592431164d4
crazzyleucocyte <alstjr7696@naver.com>
2024-05-13 10:27:03 +0900

    .

--- day04/src/_02_repetitive/T07_ex02.java
+++ day04/src/_02_repetitive/T07_ex02.java
@@ -3,8 +3,36 @@ package _02_repetitive;
 public class T07_ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		//bool p111
+		//1.
+		//1부터 100까지의 정수 중 5의 배수의 합계를 출력하는 프로그램을 작성하시오. 
+//		 
+//		아래 코드에서 반복문을 이용하여 합계 연산을 처리하는 코드를 추가하시오
+		System.out.println("1. 1부터 100까지의 정수 중 5의 배수의 합계를 출력하는 프로그램을 작성하시오.");
+		int sum=0;
+		for(int i=0;i<=100;i++) {
+			if((i%5)==0) {
+				sum+=i;
+			}
+		}
+		System.out.println("1부터 100까지 5의 배수의 합은"+sum+"입니다.");
+		
+		//혹은
+		
+		//1부터 100까지의 정수 중 5의 배수의 합계를 출력하는 프로그램을 작성하시오. 
+//		 
+//		아래 코드에서 반복문을 이용하여 합계 연산을 처리하는 코드를 추가하시오
+		System.out.println("1. 1부터 100까지의 정수 중 5의 배수의 합계를 출력하는 프로그램을 작성하시오.");
+		sum=0;
+		for(int i=5;i<=100;i+=5) {
+				sum+=i;
+		}
+		System.out.println("1부터 100까지 5의 배수의 합은"+sum+"입니다.");
+		
+		System.out.println("1부터 100까지 5의 배수의 합은"+sum+"입니다.");
+		
 	}
 
 }
