d50cbafe9db7c2a5057382bf7608645a819da8dc
crazzyleucocyte <alstjr7696@naver.com>
2024-05-13 10:09:31 +0900

    .

---
+++ selfstudy/src/selfstudy/Thing.java
@@ -0,0 +1,12 @@
+package selfstudy;
+
+public class Thing {
+
+	public static void main(String[] args) {
+	  System.out.println("2: "+Integer.toBinaryString(2));//2를 2진수로 나타낸 것 
+	  //Integer.toBinaryString()은 괄호의 숫자를 이진수로 나타내는 메서드다 
+	  
+
+	}
+
+}
