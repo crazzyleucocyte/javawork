ba7e570d18a68b5a3e70e7b135bce05c63c0eaa2
crazzyleucocyte <alstjr7696@naver.com>
2024-06-07 00:29:22 +0900

    .

--- Ex_Network/src/Network/Client.java
+++ Ex_Network/src/Network/Client.java
@@ -1,9 +1,54 @@
 package Network;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+		int port = 9001;
+		String ip;
+		try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+
+			try(
+					Socket client = new Socket(ip,port);
+					BufferedReader br=new BufferedReader(new InputStreamReader(client.getInputStream()));
+					PrintWriter pw = new PrintWriter(client.getOutputStream(),true);
+					) {
+				System.out.println("연결 성공");
+				System.out.println( br.readLine());
+
+				while(true) {
+					System.out.println("클라이언트 : ");
+					pw.println(s.nextLine());
+					pw.flush();
+					System.out.println("서버 : ");
+					System.out.println( br.readLine());
+					
+
+
+				}
+
+
+
+
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+
+
 
 	}
 
--- Ex_Network/src/Network/Server.java
+++ Ex_Network/src/Network/Server.java
@@ -1,10 +1,53 @@
 package Network;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		int port = 9001;
+		
+		Socket socket=null;
+		try(ServerSocket serverSocket = new ServerSocket(port);
+				) {
+			System.out.println("클라이언트의 접속을 기다리는 중...");
+			socket= serverSocket.accept();	//accept의 반환형이 socket이므로 소켓=서버소켓.accept를 해줘야 한다.
+			System.out.println(socket.getInetAddress().getHostAddress()+"가 접속을 요청함");
+			BufferedReader br= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter pr=new PrintWriter(socket.getOutputStream(),true);
+			
+//			InputStream is = socket.getInputStream();
+//			OutputStream os = socket.getOutputStream();
+			
+//			BufferedReader br= new BufferedReader(new InputStreamReader(is));
+//			PrintWriter pr=new PrintWriter(os,true);
+			
+			pr.println("환영합니다.");
+			while(true) {
+				System.out.println("클라이언트 : ");
+				System.out.println( br.readLine());
+				
+				System.out.println("서버 : ");
+				pr.println(s.nextLine());
+				pr.flush();
+			}
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
 	}
 
 }
